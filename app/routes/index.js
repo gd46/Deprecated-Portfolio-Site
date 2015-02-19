dbac93739eb3380ae45fe414a4e9e02e2c6ce2bd
Giuseppe DiBella <gd46@njit.edu>
2015-02-19 01:25:24 -0500

    Added contact page link to new template and added new background images

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Parallax Template - Materialize' });
+  res.render('index', { title: 'Giuseppe DiBellas Portfolio Site' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Page' });
 });
 
 module.exports = router;
