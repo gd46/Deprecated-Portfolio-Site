1c06e90f2ccce3049f846e0a84bb30efe72c99bd
Giuseppe DiBella <gd46@njit.edu>
2015-03-05 21:14:58 -0500

    Clean up

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Giuseppe DiBellas Portfolio Site' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Page' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Page' });
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'My Work'});
+});
+
+router.get('/blog', function(req, res, next) {
+  res.render('blog', { title: 'Blog'});
+});
+
+module.exports = router;
