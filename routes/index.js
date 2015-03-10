2ae9ffff3344a5ae47a87ce37e8b0783f35ffe4c
Giuseppe DiBella <gd46@njit.edu>
2015-03-10 19:36:31 -0400

    Commented out the blog and contact pages for now

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ router.get('/about', function(req, res, next) {
 });
 
 router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'My Work'});
+  res.render('projects', { title: 'My Projects'});
 });
 
 router.get('/blog', function(req, res, next) {
